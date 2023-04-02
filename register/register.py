9bf96a39261a627f47913c1f1c9c496d056f78d1
ali taghavian <taghavianali@gmail.com>
2023-04-02 18:21:54 +0430

    register question is solved

---
+++ register/register.py
@@ -0,0 +1,12 @@
+from re import *
+
+def check_registration_rules(**kwargs):
+    a=kwargs.copy()
+    for i in kwargs:
+        if len(kwargs[i])<6 or len(i)<4 or i=='quera' or i=='codecup' or   match("^\d+$",i):
+            a.pop(i)
+    
+    return list(a.keys())
+        
+# b=check_registration_rules(username='password', sadegh='He3@lsa')
+check_registration_rules()
--- tests.py
+++ tests.py
@@ -1,26 +1,7 @@
-# print(*[x for x in sorted([x for x in map(int, input().split()[5::6]) if x % 6 == 0])])
+def f(*args, **kwargs):
+     print(args)
+     print(kwargs)
 
-def f(a, i):
-    if i==1:
-        L="ali"
-    else:
-        L.append("ali")
-    return L
-
-# def calc(a: list) -> tuple:
-#     n=len(a)
-#     a.sort()
-#     ave=sum(a)/len(a)
-    
-#     if n%2==0:
-#         miane=sum(a[int(n/2)-1:int(n/2)+1])/2
-#     else:
-#         miane=a[n//2]
-#     maxi=max(a)
-#     return ave,miane,maxi
-
-# calc([2,20,30,29])
-# dict
-print(f(1,1))
-print(f(2,2))
-print(f(3,3))
+f(1,a=1)
+a=lambda x:x**2
+print(a(2))
